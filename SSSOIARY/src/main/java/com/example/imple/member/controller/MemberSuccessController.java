256f570c64d5e687b49fd1e5d7860c538d46dd45
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 <prttysso@gmail.com>
2023-06-23 06:34:03 +0900

    2023/06/23

--- SSSOIARY/src/main/java/com/example/imple/member/controller/MemberSuccessController.java
+++ SSSOIARY/src/main/java/com/example/imple/member/controller/MemberSuccessController.java
@@ -1,9 +1,11 @@
 package com.example.imple.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.imple.member.color.mapper.MemberColorMapper;
 import com.example.standard.controller.SuccessController;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,8 +14,16 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping("/member")
 public class MemberSuccessController implements SuccessController {
 
+	@Autowired
+	MemberColorMapper colorMapper;
+	
 	@Override
 	public void success(Model model, HttpServletRequest request) {
+		
+		var session = request.getSession();
+		var name = (String)session.getAttribute("name");
+		var color = colorMapper.selectColorByName(name);
+		model.addAttribute("color", color);
 		
 	}
 
